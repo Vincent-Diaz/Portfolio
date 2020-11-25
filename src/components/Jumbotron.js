2fa85df32dcfa007f07370651d709a8085684db8
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-11-24 23:24:34 -0600

    added jumbotron component

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -6,9 +6,9 @@ function Jumbotron() {
             <div className="containerj">
                 <h1 className="display-4">Welcome to My Projects!</h1>
                 <p className="lead">I am currently a student in the University of Texas Coding Bootcamp. Below is some of my previous
-        projets that I am very proud of. Thank you for stopping by. Enjoy!</p>
+                projets that I am very proud of. Thank you for stopping by. Enjoy!</p>
             </div>
         </header>
     )
-} 
+}
 export default Jumbotron
