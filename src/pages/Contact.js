0097527926c82bdccb82ecb15f1265099474044c
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-11-24 23:44:06 -0600

    added contact component from html

---
+++ src/pages/Contact.js
@@ -0,0 +1,39 @@
+import React from "react"
+import Container from "../components/Container"
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
+
+function Contact() {
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col size="lg-12">
+                        <h1>Contact</h1>
+                        <ul>
+                            <li>Email: diazkik@yahoo.com</li>
+                            <li>Mobile: (806)-282-1662</li>
+                        </ul>
+                        <hr />
+                    </Col>
+                </Row>
+                <form>
+                    <div className="form-group">
+                        <label for="exampleInputName1">Name</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <div className="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" />
+                        </div>
+                        <div className="form-group">
+                            <label for="exampleFormControlTextarea1">Message</label>
+                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </Container>
+        </div>
+    )
+}
