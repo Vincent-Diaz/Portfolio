d09efc9a9248cdc8e47b9bbe6a5e452ec60fb87c
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-11-25 01:34:46 -0600

    imported resume pdf changed Link back to a tags

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,9 @@
 import React from "react"
-import { Link } from "react-router-dom";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
-import Img from "../../images/mainphoto.jpg"
+import Img from "../images/mainphoto.jpg"
+import Resume from "../Vincent_Diaz_Resume.pdf"
 
 function About() {
     return (
@@ -18,17 +18,17 @@ function About() {
                 <Row>
                     <Col size="lg-3">
                         <img src={Img} alt="headshot" className="img-thumbnail" />
-                        <Link className="link1" href="https://github.com/Vincent-Diaz" target="_blank" rel="noopener noreferrer">
+                        <a className="link1" href="https://github.com/Vincent-Diaz" target="_blank" rel="noopener noreferrer">
                             Link to Github Profile
-                        </Link>
+                        </a>
                         <hr />
-                        <Link className="link2" href="https://www.linkedin.com/in/vincent-diaz-89a4609b/" target="_blank" rel="noopener noreferrer">
+                        <a className="link2" href="https://www.linkedin.com/in/vincent-diaz-89a4609b/" target="_blank" rel="noopener noreferrer">
                             Link to Linkedin Profile
-                        </Link>
+                        </a>
                         <hr />
-                        <Link className="link3" href="./Vincent_Diaz_Resume.pdf" target="_blank" rel="noopener noreferrer">
+                        <a className="link3" href={Resume} target="_blank" rel="noopener noreferrer">
                             Link to Resume
-                        </Link>
+                        </a>
                     </Col>
                     <Col size="lg-9">
                         <p>Welcome to my portfolio page! Now that you are here, let me tell you a bit about myself. I am currently enrolled at the University of Texas in Austin Coding Bootcamp. I'm an aspiring Web Developer pursing a new career path. I was born and raised in Amarillo, TX a smaller city in the West Texas. Originally I going to relocate to Austin for this bootcamp until the in-class option was canceled due to the COVID-19 pandemic. But, that did not stop me from pursing something that has become a passion of mine. The plan is to move to a bigger city after graduating from the program. I graduated from Caprock High in 2013. I went on to attend West Texas A&M in Canyon, TX for two and a half semesters.  </p>
